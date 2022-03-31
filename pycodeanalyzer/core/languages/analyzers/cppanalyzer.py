d8de1188d86768912f8e9519a006d412bf4d73e5
Mion Giovanni <giovanni.mion@faurecia.com>
2022-03-31 17:22:12 +0200

    remove unnecessary param and fix typo

--- pycodeanalyzer/core/languages/analyzers/cppanalyzer.py
+++ pycodeanalyzer/core/languages/analyzers/cppanalyzer.py
@@ -150,10 +150,10 @@ class CppAnalyzer(Analyzer):
         self.objectPaths: List[str] = []
         self.encoding: Encoding = Encoding()
         self.forceIgnoredSymbols: List[str] = []
-        self.configuation = config
+        self.configuration = config
         self.configured = False
         self.defines: List[str] = []
-        self.defineConfig(config)
+        self.defineConfig()
 
     def analyze(self, rootDir: str, path: str) -> List[AbstractObject]:
         abstractObjects: List[AbstractObject] = []
@@ -344,14 +344,14 @@ class CppAnalyzer(Analyzer):
         token = token[: token.index(endDelim) - 1].replace("'", "").strip()
         return token
 
-    def defineConfig(self, config: Configuration) -> None:
-        config.defineConfig(
-            "parser.CPP",
+    def defineConfig(self) -> None:
+        self.configuration.defineConfig(
+            "Parser.CPP",
             "defines",
             'All value to be define when preprosession : List of name[ value]. Example : ["toto 1","titi \'hello\'", "tata"]',
         )
 
     def handleConfigation(self) -> None:
-        defines = self.configuation.get("parser.CPP", "defines")
+        defines = self.configuration.get("Parser.CPP", "defines")
         if defines:
             self.defines = json.loads(defines)
